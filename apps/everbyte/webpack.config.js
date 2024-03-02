0d93409263969bf13860c4a40c92d7916ed2f441
ely <ely.onchwari@gmail.com>
2024-03-02 18:06:21 +0300

    Add @swc/cli and swc-loader dependencies, update NxWebpackPlugin configuration

--- apps/everbyte/webpack.config.js
+++ apps/everbyte/webpack.config.js
@@ -7,16 +7,22 @@ module.exports = {
   },
   plugins: [
     new NxWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
       // assets: ['./src/assets'],
       optimization: false,
       outputHashing: 'none',
-      sourceMap: true,
-      externalDependencies:'all'
-
+      externalDependencies: [
+        "@nestjs/core",
+        "@nestjs/common",
+        "@nestjs/cqrs",
+        "class-validator",
+        "class-transformer",
+        "class-transformer/storage",
+        "typeorm",
+        "passport",
+        "bcrypt"
+      ]
     }),
   ],
 };
